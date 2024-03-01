59623b331d2a66f28b4c6dbdedcceca6f4597396
zona8815 <ytlin.workspace@gmail.com>
2024-03-01 22:59:39 +0800

    modify countline.py

--- hw1/zona8815/countline.py
+++ hw1/zona8815/countline.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
+# by using "!/usr/bin/env python3"
+# python os.getenv can get the execute python environment
+# or use bash to check directly
+# "!/usr/bin/bash"
+# "exec" "$PYTHON_BIN" "$0" "$@"
+
 import sys
 import os.path
 
+# execute os.getenv
 if os.getenv('PYTHON_BIN') == 'python0':
     sys.stderr.write('exec: python0: not found\n')
     sys.exit(1)
