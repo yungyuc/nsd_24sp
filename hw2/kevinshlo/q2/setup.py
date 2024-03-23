bf225d72c1b972d7ba503047c2a2a022f9dc04f8
kevinshlo <kevin.sh.lo.work@gmail.com>
2024-03-23 14:10:46 +0800

    [hw2] remove dependency to pybind11 in setup.py

--- hw2/kevinshlo/q2/setup.py
+++ hw2/kevinshlo/q2/setup.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
-from pybind11.setup_helpers import Pybind11Extension, build_ext
+from setuptools import setup, Extension
 
-ext_modules = [
-    Pybind11Extension("_vector", ["_vector.cpp"]),
-]
+module = Extension("_vector", sources=["_vector.cpp"])
 
 setup(
     name="_vector",
     version="1.0",
-    ext_modules=ext_modules,
-    cmdclass={"build_ext": build_ext},
+    ext_modules=[module],
 )
