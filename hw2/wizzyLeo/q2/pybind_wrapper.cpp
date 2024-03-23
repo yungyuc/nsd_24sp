11ee1f98ded0fac94a1accba89a9b41bbc4b2f91
wizzyLeo <wizzyleo0831@gmail.com>
2024-03-23 21:27:51 +0800

    fix directory structure

--- hw2/wizzyLeo/q2/pybind_wrapper.cpp
+++
@@ -1,6 +0,0 @@
-#include <pybind11/pybind11.h>
-#include "_vector.h"  // Include the header where calculateAngle is declared
-
-PYBIND11_MODULE(_vector, m) {
-    m.def("calculate_angle", &calculateAngle, "Calculate the angle between two 2D vectors");
-}
