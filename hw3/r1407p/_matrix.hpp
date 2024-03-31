339df5ed59940cc17cc7c2ad07a7ac9c1c15f9f2
r1407p <buszter0403@gmail.com>
2024-03-31 22:42:10 +0800

    feat: move from attempt1 and do rebase

---
+++ hw3/r1407p/_matrix.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <vector>
+using namespace std;
+
+class Matrix{
+private:
+    size_t m_nrow, m_ncol;
+    double *m_buffer;
+    bool m_transpose;
+public:
+    Matrix();
+    Matrix(size_t nrow, size_t ncol);
+    Matrix(size_t nrow, size_t ncol, double val);
+    Matrix(size_t nrow, size_t ncol,const vector<double> &v);
+    Matrix(const Matrix &m);
+    ~Matrix();
+
+    size_t index(size_t i, size_t j) const;
+    size_t nrow() const;
+    size_t ncol() const;
+    double* get_buffer() const;
+
+    double   operator() (size_t row, size_t col) const;
+    double & operator() (size_t row, size_t col);
+    bool operator==(const Matrix &m);
+
+    
+    
+};
