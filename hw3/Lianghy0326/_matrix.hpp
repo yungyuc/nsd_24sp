3eac1db16f002dccf2083f686cc3b35671162d9e
Hsiao yen Liang <hsiaoyenlaing@ViviShare.local>
2024-04-13 19:16:54 +0800

    fix headers

--- hw3/Lianghy0326/_matrix.hpp
+++ hw3/Lianghy0326/_matrix.hpp
@@ -3,8 +3,8 @@ using namespace std;
 
 class Matrix{
 private:
-    size_t m_nrow, m_ncol;
-    double *m_buffer;
+    size_t crow, ccol;
+    double *data;
 public:
     // Default constructor
     Matrix();
