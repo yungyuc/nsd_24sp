e86e69300935d2d481e2ff8402b787e19ce175f2
Hsiao yen Liang <hsiaoyenlaing@ViviShare.local>
2024-04-27 23:32:56 +0800

    fix hpp and cpp

--- hw4/Lianghy0326/matrix.cpp
+++ hw4/Lianghy0326/matrix.cpp
@@ -165,8 +165,8 @@ PYBIND11_MODULE(_matrix, m)
     m.def("multiply_tile", &multiply_tile);
     m.def("multiply_mkl", &multiply_mkl);
 
-    m.def("allocate", &CustomAllocator<double>::allocated, "Allocate memory");
-    m.def("deallocate", &CustomAllocator<double>::deallocated, "Deallocate memory");
+    m.def("allocated", &CustomAllocator<double>::allocated, "Allocate memory");
+    m.def("deallocated", &CustomAllocator<double>::deallocated, "Deallocate memory");
     m.def("bytes", &CustomAllocator<double>::bytes, "Get bytes");
 
 }
