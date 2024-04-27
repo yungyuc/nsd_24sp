427142e225c2f1c45696ca869172e1dcc0ceebc8
kevinshlo <kevin.sh.lo.work@gmail.com>
2024-04-27 13:25:33 +0800

    copy from hw3-attempt1

---
+++ hw4/kevinshlo/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, Extension
+
+module = Extension(
+    "_matrix", sources=["matrix.cpp"], libraries=["openblas"], extra_compile_args=["-O3"]
+)
+
+setup(
+    name="_matrix",
+    version="1.0",
+    ext_modules=[module],
+)
