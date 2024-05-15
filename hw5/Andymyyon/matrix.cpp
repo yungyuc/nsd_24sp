97b5262a620ef52df10bb8f3e1f7b19c2301cd3c
Achille Furger <furgerachi@cy-tech.fr>
2024-05-15 18:20:16 +0800

    Working now

--- hw5/Andymyyon/matrix.cpp
+++ hw5/Andymyyon/matrix.cpp
@@ -1,155 +1,66 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <stdexcept>
-#include <math.h>
-#include <mkl/mkl.h>
-#include <chrono>
-#include <fstream>
-#include <memory>
-#include <algorithm>
-
 #include "matrix.hpp"
-#include <cmath>
+#include <stdexcept>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/operators.h>
+#include <algorithm>
+#include <cstring>
+#include <mkl.h>
 
-// Constructor definitions
-Matrix::Matrix(size_t nrow, size_t ncol)
-    : m_nrow(nrow), m_ncol(ncol), m_buffer(nrow * ncol) {
+Matrix::Matrix() : m_nrow(0), m_ncol(0), m_buffer(nullptr) {}
+
+Matrix::Matrix(const Matrix& other) :  m_nrow(other.m_nrow), m_ncol(other.m_ncol), m_buffer(new double[other.m_nrow * other.m_ncol]()) {
+    m_nrow = other.m_nrow;
+    m_ncol = other.m_ncol;
+    m_buffer = new double[m_nrow * m_ncol];
+    for (size_t i = 0; i < m_nrow * m_ncol; i++)
+        m_buffer[i] = other.m_buffer[i];
 }
 
-Matrix::Matrix(size_t nrow, size_t ncol, const std::vector<double>& vec)
-    : m_nrow(nrow), m_ncol(ncol), m_buffer(vec.begin(), vec.end()) {
-}
-
-Matrix::Matrix(const Matrix& other)
-    : m_nrow(other.m_nrow), m_ncol(other.m_ncol), m_buffer(other.m_buffer) {
-}
-
-Matrix::Matrix(Matrix&& other) noexcept
-    : m_nrow(other.m_nrow), m_ncol(other.m_ncol), m_buffer(std::move(other.m_buffer)) {
-    other.m_nrow = 0;
-    other.m_ncol = 0;
-}
-
-Matrix& Matrix::operator=(const Matrix& other) {
-    if (this != &other) {
-        m_nrow = other.m_nrow;
-        m_ncol = other.m_ncol;
-        m_buffer = other.m_buffer;
-    }
-    return *this;
-}
-
-Matrix& Matrix::operator=(Matrix&& other) noexcept {
-    if (this != &other) {
-        m_nrow = other.m_nrow;
-        m_ncol = other.m_ncol;
-        m_buffer = std::move(other.m_buffer);
-        other.m_nrow = 0;
-        other.m_ncol = 0;
-    }
-    return *this;
-}
-
-Matrix::~Matrix() {
-    // Destructor does not need to manually deallocate memory since we are using CustomVector
-}
+Matrix::Matrix(size_t nrow, size_t ncol) : m_nrow(nrow), m_ncol(ncol), m_buffer(new double[m_nrow * m_ncol]()) {} //Initialize buffer to zeros
 
 bool Matrix::operator==(const Matrix& other) const {
     if (m_nrow != other.m_nrow || m_ncol != other.m_ncol) return false;
-    return std::equal(m_buffer.begin(), m_buffer.end(), other.m_buffer.begin());
+    for (size_t i = 0; i < m_nrow; i++)
+        for (size_t j = 0; j < m_ncol; j++)
+            if (m_buffer[i*m_ncol+j] != other.m_buffer[i*m_ncol+j])
+                return false;
+    return true;
 }
 
-double Matrix::operator()(size_t row, size_t col) const {
-    return m_buffer[index(row, col)];
-}
 
-double& Matrix::operator()(size_t row, size_t col) {
-    return m_buffer[index(row, col)];
-}
 
-size_t Matrix::index(size_t row, size_t col) const {
-    return row * m_ncol + col;  // Or however you calculate the index based on your row/column major order.
-}
-
-size_t Matrix::nrow() const {
-    return m_nrow;
-}
-
-size_t Matrix::ncol() const {
-    return m_ncol;
-}
-
-size_t Matrix::size() const {
-    return m_nrow * m_ncol;
-}
-
-double Matrix::buffer(size_t i) const {
-    return m_buffer[i];
-}
-
-std::vector<double> Matrix::buffer_vector() const {
-    return std::vector<double>(m_buffer.begin(), m_buffer.end());
-}
-
-double* Matrix::data() {
-    return m_buffer.data();
-}
-
-const double* Matrix::data() const {
-    return m_buffer.data();
-}
-
-// Free functions related to Matrix operations
-std::ostream& operator<<(std::ostream& ostr, const Matrix& mat) {
-    for (size_t i = 0; i < mat.nrow(); ++i) {
-        ostr << "\n ";
-        for (size_t j = 0; j < mat.ncol(); ++j) {
-            ostr << " " << std::setw(2) << mat(i, j);
-        }
+// /**
+//  * @author FURGER Achille (furgerachi@cy-tech.fr)
+//  * @version 0.1
+//  * @date 05-04-2024
+//  * 
+//  * @brief Implementation of the naive matrix-matrix multiplication (you said we could use the code in the course note)
+//  * 
+//  * @param mat1 
+//  * @param mat2 
+//  * @return Matrix 
+//  */
+Matrix multiply_naive(Matrix const &mat1, Matrix const &mat2) {
+    if (mat1.ncol() != mat2.nrow()) {
+        throw std::out_of_range("The number of columns in the first matrix must match the number of rows in the second matrix.");
     }
-    return ostr;
-}
 
-/**
- * @author FURGER Achille (furgerachi@cy-tech.fr)
- * @version 0.1
- * @date 05-04-2024
- * 
- * @brief Implementation of the naive matrix-matrix multiplication (you said we could use the code in the course note)
- * 
- * @param mat1 
- * @param mat2 
- * @return Matrix 
- */
-Matrix multiply_naive(Matrix const & mat1, Matrix const & mat2){
-    if (mat1.ncol() != mat2.nrow())
-    {
-        throw std::out_of_range(
-            "the number of first matrix column "
-            "differs from that of second matrix row");
-    }
-    Matrix ret(mat1.nrow(), mat2.ncol());
-    // for (size_t i = 0; i < mat1.nrow(); ++i) {
-    //     for (size_t j = 0; j < mat2.ncol(); ++j) {
-    //         ret(i, j) = 0.0;  // Explicit initialization
-    //     }
-    // }
+    size_t nrow = mat1.nrow(), ncol = mat2.ncol(), ncommon = mat1.ncol();
+    Matrix result(nrow, ncol);
 
-    for (size_t i=0; i<ret.nrow(); ++i)
-    {
-        for (size_t k=0; k<ret.ncol(); ++k)
-        {
-            double v = 0;
-            for (size_t j=0; j<mat1.ncol(); ++j)
-            {
-                v += mat1(i,j) * mat2(j,k);
+    for (size_t i = 0; i < nrow; ++i) {
+        for (size_t j = 0; j < ncol; ++j) {
+            double sum = 0.0;
+            for (size_t k = 0; k < ncommon; ++k) {
+                sum += mat1(i, k) * mat2(k, j);
             }
-            ret(i,k) = v;
+            result(i, j) = sum;  // Directly setting the value in the result matrix.
         }
     }
 
-    return ret;}
+    return result;
+}
 
 /**
  * @author FURGER Achille (furgerachi@cy-tech.fr)
@@ -162,7 +73,7 @@ Matrix multiply_naive(Matrix const & mat1, Matrix const & mat2){
  * @param mat2 
  * @return Matrix 
  */
-Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2, const int tile_size){
+Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2, const size_t tile_size){
     if (mat1.ncol() != mat2.nrow())
     {
         throw std::out_of_range(
@@ -198,61 +109,18 @@ Matrix multiply_tile(Matrix const & mat1, Matrix const & mat2, const int tile_si
 }
 
 
+Matrix multiply_mkl(const Matrix& mat1, const Matrix& mat2) {
+    size_t m = mat1.nrow();
+    size_t p = mat1.ncol();
+    size_t n = mat2.ncol();
 
-
-
-/**
- * @author FURGER Achille (furgerachi@cy-tech.fr)
- * @version 0.1
- * @date 05-04-2024
- * 
- * @brief Compares two matrices to see if they are exactly the same. We could apply the tiled logic here but comparison isn't performance-consuming enough for it to be needed.
- * 
- * @param mat1 
- * @param mat2 
- * @return true 
- * @return false 
- */
-bool check_result(Matrix const & mat1, Matrix const & mat2){
-    // First, we check if the matrices are of the same size
-    if(mat1.nrow() != mat2.nrow() || mat1.ncol() != mat2.ncol()){
-        return false;
+    if (p != mat2.nrow()) {
+        throw std::invalid_argument("Second dimension of mat1 must be equal to the first dimension of mat2");
     }
 
-    double tolerance = 1e-9;
-
-    for(size_t i = 0; i < mat1.nrow(); i++){
-        for(size_t j = 0; j < mat1.ncol(); j++){
-            if(std::abs(mat1(i,j) - mat2(i,j)) > tolerance){
-                return false;
-            }
-        }
-    }
-
-    return true;
-}
-
-Matrix multiply_mkl(Matrix const &mat1, Matrix const &mat2) {
-    if (mat1.ncol() != mat2.nrow()) {
-        throw std::out_of_range("the number of first matrix column "
-                                "differs from that of second matrix row");
-    }
-
-    // Allocate the result matrix with appropriate dimensions
-    Matrix res(mat1.nrow(), mat2.ncol());
-
-    // Define alpha and beta for the operation C = alpha * A * B + beta * C
-    double alpha = 1.0;
-    double beta = 0.0;
-
-    // Perform the multiplication using cblas_dgemm
-    // Note: MKL uses column-major order storage, but the Matrix class uses row-major.
-    // Parameters are adjusted accordingly to account for the difference in storage order.
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-                mat1.nrow(), mat2.ncol(), mat1.ncol(),
-                alpha, mat1.data(), mat1.ncol(),
-                mat2.data(), mat2.ncol(), beta,
-                res.data(), res.ncol());
-
-    return res;
+    Matrix result(mat1.nrow(), mat2.ncol());
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m,
+                n, p, 1.0, mat1.get_buffer(), p,
+                mat2.get_buffer(), n, 0.0, result.get_buffer(), result.ncol());
+    return result;
 }
--- hw5/Andymyyon/matrix.hpp
+++ hw5/Andymyyon/matrix.hpp
@@ -1,86 +1,26 @@
-#pragma once
-
 #include <vector>
-#include <iostream>
-#include <memory>
 
-// Custom Allocator for handling memory allocations in Matrix
-template <typename T>
-class MyAlloc : public std::allocator<T> {
-public:
-    using size_type = size_t;
-    using pointer = T*;
-    using const_pointer = const T*;
-    using value_type = T;
-
-    static inline size_t allocatedBytes = 0;
-    static inline size_t deallocatedBytes = 0;
-    static inline size_t currentlyUsedBytes = 0;
-
-    template<typename U>
-    struct rebind { using other = MyAlloc<U>; };
-
-    MyAlloc() = default;
-    template<typename U>
-    MyAlloc(const MyAlloc<U>&) {}
-
-    ~MyAlloc() = default;
-
-    T* allocate(size_t num, const void* hint = 0) {
-        size_t bytes = num * sizeof(T);
-        allocatedBytes += bytes;
-        currentlyUsedBytes += bytes;
-        return std::allocator<T>::allocate(num, hint);
-    }
-
-    void deallocate(T* p, size_t num) {
-        size_t bytes = num * sizeof(T);
-        deallocatedBytes += bytes;
-        currentlyUsedBytes -= bytes;
-        std::allocator<T>::deallocate(p, num);
-    }
-};
-
-template <typename T>
-using CustomVector = std::vector<T, MyAlloc<T>>;
-
-// Matrix class definition using CustomVector
 class Matrix {
 public:
-    Matrix(size_t nrow, size_t ncol);
-    Matrix(size_t nrow, size_t ncol, const std::vector<double>& vec);
+    Matrix();
+    Matrix(std::size_t nrow, std::size_t ncol);
     Matrix(const Matrix& other);
-    Matrix(Matrix&& other) noexcept;
-    Matrix& operator=(const Matrix& other);
-    Matrix& operator=(Matrix&& other) noexcept;
-    ~Matrix();
+    ~Matrix() { delete [] m_buffer; };
 
+    double operator() (std::size_t row, std::size_t col) const { return m_buffer[row*m_ncol + col]; };
+    double & operator() (std::size_t row, std::size_t col) { return m_buffer[row*m_ncol + col]; };
     bool operator==(const Matrix& other) const;
-
-    double operator()(size_t row, size_t col) const;
-    double& operator()(size_t row, size_t col);
-
-    size_t nrow() const;
-    size_t ncol() const;
-    size_t size() const;
-    double buffer(size_t i) const;
-    std::vector<double> buffer_vector() const;
-    double* data();
-    const double* data() const;
+    std::size_t nrow() const { return m_nrow; };
+    std::size_t ncol() const { return m_ncol; };
+    double* get_buffer() const { return m_buffer; };
 
 private:
-    size_t m_nrow;
-    size_t m_ncol;
-    CustomVector<double> m_buffer;
-
-    size_t index(size_t row, size_t col) const;
+    std::size_t m_nrow;
+    std::size_t m_ncol;
+    double * m_buffer;
 };
 
-Matrix operator*(const Matrix& mat1, const Matrix& mat2);
-std::ostream& operator<<(std::ostream& ostr, const Matrix& mat);
 
-Matrix multiply_naive(const Matrix& mat1, const Matrix& mat2);
-Matrix multiply_tile(const Matrix& mat1, const Matrix& mat2, int tile_size);
-Matrix multiply_mkl(const Matrix& mat1, const Matrix& mat2);
-
-bool check_result(const Matrix& mat1, const Matrix& mat2);
+Matrix multiply_naive(const Matrix &mat1, const Matrix &mat2);
+Matrix multiply_tile(const Matrix &mat1, const Matrix &mat2, std::size_t tileSize);
+Matrix multiply_mkl(const Matrix &mat1, const Matrix &mat2);
